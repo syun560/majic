6a0475d9e10bc7967954df8dfa5d483a541ca841
syun560 <bajikoji@gmail.com>
2023-11-15 17:59:17 +0900

    update

--- app/points.ts
+++ app/points.ts
@@ -6,13 +6,52 @@ export const points = [
       parent_ron: 2000,
       aka: "30符1翻",
       ex1: "",
-      rate: 0.4
+      rate: 0.2
+    },
+    {
+      child: 400,
+      parent: 700,
+      ron: 1000,
+      aka: "テンパネ？",
+      ex1: "",
+      rate: 0.2
     },
     {
       child: 400,
       parent: 800,
       ron: 1000,
       aka: "テンパネ？",
+      ex1: "",
+      rate: 0.01
+    },
+    {
+      child: 700,
+      parent: 1300,
+      ron: 1600,
+      aka: "25符",
+      ex1: "七対子",
+      rate: 0.1
+    },
+    {
+      child: 1000,
+      parent: 2000,
+      ron: 4000,
+      aka: "30符3翻",
+      ex1: "",
+      rate: 0.2
+    },
+    {
+      child: 1300,
+      parent: 2600,
+      ron: 5200,
+      aka: "リーチ",
+      ex1: "平和ツモ",
+      rate: 0.4
+    },
+    {
+      child: 2000,
+      parent: 3900,
+      aka: "30符4翻",      
       ex1: "",
       rate: 0.4
     },
@@ -26,7 +65,37 @@ export const points = [
     {
       child: 3000,
       parent: 6000,
+      child_ron: 12000,
+      parent_ron: 18000,
       aka: "跳満",
+      ex1: "",
       rate: 0.2
-    }
-  ]
+    },
+    {
+      child: 4000,
+      parent: 8000,
+      child_ron: 1000,
+      parent_ron: 36000,
+      aka: "倍満",
+      ex1: "",
+      rate: 0.1
+    },
+    {
+      child: 6000,
+      parent: 12000,
+      child_ron: 24000,
+      parent_ron: 36000,
+      aka: "三倍満",
+      ex1: "",
+      rate: 0.01
+    },
+    {
+      child: 8000,
+      parent: 16000,
+      child_ron: 32000,
+      parent_ron: 48000,
+      aka: "数え役満",
+      ex1: "",
+      rate: 0.02
+    },
+  ];
